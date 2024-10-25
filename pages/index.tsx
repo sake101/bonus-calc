60904d51637291f5792e2bc60dacfa903439b991
sake101 <william@coloradosakeco.com>
2024-10-25 09:08:14 -0600

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <div>Hello! Your bonus calculator will appear here.</div>
+    </main>
+  )
+}
